bccabc90f377dfac3fe9dc986384b58cbcf740ad
vineeth-119 <vineethsharma30@gmail.com>
2024-02-07 23:55:01 +0530

    Web socket client tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebSocketClient';
+  @Input() stock:any;
+  private webSocket: WebSocket;
+
+  constructor(){
+    this.webSocket = new WebSocket('ws://localhost:8080/stocks');
+    console.log('done');
+    this.webSocket.onmessage=(event)=>{
+        this.stock = JSON.parse(event.data);
+        console.log(this.stock);
+    };
+  }
+
+}
+export interface Stock{
+  name:String,
+  amount:number,
+  increased:boolean,
+  icon:string
 }
